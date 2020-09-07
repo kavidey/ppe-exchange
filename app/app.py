8f121994c7f4734792ac60e3ab4c9f6ec63937c2
KaviMD <kaviasher@gmail.com>
2020-09-07 16:48:23 -0700

    Enable foreign_keys

--- app/app.py
+++ app/app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
 from flask_bootstrap import Bootstrap
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 # HACK
 def make_app(name):
