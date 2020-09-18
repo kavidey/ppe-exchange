7ac26e032174ffe981fab755fa2d3dfc97148516
Jenna-LI <58526103+Jenna-LI@users.noreply.github.com>
2020-09-18 16:54:18 -0700

    removed edit user profile when logged out.

--- app/routes.py
+++ app/routes.py
@@ -175,7 +175,7 @@ def register():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Congratulations, you are now a registered user!')
+        flash('Congratulations, you are now a registered user! Please wait for the admin to verify your account to access this site. You should recieve a verification email shortly')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
